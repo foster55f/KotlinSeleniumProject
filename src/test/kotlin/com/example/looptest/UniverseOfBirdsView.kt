2b2c7af845bb26df7216c1af0696582ffd92ea90
Foster Taylor <taylorf2@aetna.com>
2021-09-06 14:56:28 -0600

    Updates element with new title

--- src/test/kotlin/com/example/looptest/UniverseOfBirdsView.kt
+++ src/test/kotlin/com/example/looptest/UniverseOfBirdsView.kt
@@ -8,7 +8,7 @@ class UniverseOfBirdsView {
     val addToCartButton = element("button[id='AddToCart-product-template']")
     val catalogMenuButton = element(byXpath("/html/body/div[2]/div/header/div/nav/ul/li[2]/a"))
     val viewCartButton = element(byXpath("/html/body/div[2]/div/header/div/div[2]/div/a"))
-    val fosterBirdsProduct = element(Selectors.byText("Foster Birds!"))
+    val fosterBirdsProduct = element(Selectors.byText("Foster Birds"))
     val yourCartHeader = element(Selectors.byText("Your cart"))
     val homePageProduct = element("div[class='h4 grid-view-item__title']")
     val shoppingCartCountIcon = element(byXpath("//html/body/div[2]/div/header/div/div[2]/div/a/div/span[1]"))
